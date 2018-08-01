06ea34e1e8b5664a1a17dc4da7e07191f1a84833
fuy3 <Yu.Fu3@emc.com>
2018-08-01 17:50:34 +0800

    update weight value for each matrix

--- gen_dict/merge_matrix.py
+++ gen_dict/merge_matrix.py
@@ -7,13 +7,15 @@ utils_path = os.path.dirname(os.path.abspath(__file__)) + "/../utils"
 sys.path.append(utils_path)
 import sparse_matrix
 
-MATRIX_W1 = 0.3
+MATRIX_W1 = 0.2
 MATRIX_W2 = 0.4
 MATRIX_W3 = 1.0 - MATRIX_W1 - MATRIX_W2
 
 ww1 = sparse_matrix.load_sparse_csr("./output/w_w1.npy.npz").toarray()
 ww2 = sparse_matrix.load_sparse_csr("./output/w_w2.npy.npz").toarray()
 ww3 = sparse_matrix.load_sparse_csr("./output/w_w3.npy.npz").toarray()
+
+ww3 = np.sqrt(ww3)
 
 print(ww1.shape)
 print(ww2.shape)
